88cd6d9614f33880b46ded159feb7fd1c84b0fab
usagi <kou1992@gmail.com>
2023-09-20 14:22:42 +0900

    update

--- Sources/SwiftBitboard/Point.swift
+++ Sources/SwiftBitboard/Point.swift
@@ -1,8 +1,21 @@
 import Foundation
 
 public struct Point: Pointable {
-  public let file: Int 
-  public let rank: Int
+  public var file: Int 
+  public var rank: Int
+  
+  public mutating func move(file: Int) {
+    self.file += file
+  }
+  
+  public mutating func move(rank: Int) {
+    self.file += rank
+  }
+  
+  public mutating func move(point: Self) {
+    self.file += point.file
+    self.rank += point.rank
+  }
   
   public static func ==(lhs: Point, rhs: Point) -> Bool {
     return lhs.file == rhs.file && lhs.rank == rhs.rank
