96794c61bf6d3b2dee86520277dac5d0a28ec042
usagi <kou1992@gmail.com>
2023-09-20 14:29:52 +0900

    update

--- Sources/SwiftBitboard/Point.swift
+++ Sources/SwiftBitboard/Point.swift
@@ -4,17 +4,9 @@ public struct Point: Pointable {
   public var file: Int 
   public var rank: Int
   
-  public mutating func move(file: Int) {
-    self.file += file
-  }
-  
-  public mutating func move(rank: Int) {
-    self.file += rank
-  }
-  
-  public mutating func move(point: Self) {
-    self.file += point.file
-    self.rank += point.rank
+  public init(file: Int, rank: Int) {
+    self.file = file
+    self.rank = rank
   }
   
   public static func ==(lhs: Point, rhs: Point) -> Bool {
--- Sources/SwiftBitboard/Pointable.swift
+++ Sources/SwiftBitboard/Pointable.swift
@@ -3,9 +3,23 @@ import Foundation
 public protocol Pointable: Equatable, Comparable, Hashable {
   var file: Int { get }
   var rank: Int { get }
+  
+  init(file: Int, rank: Int)
 }
 
 extension Pointable {
+  
+  public mutating func move_to<Point :Pointable>(file: Int) -> Point {
+    .init(file: self.file + file, rank: self.rank)
+  }
+  
+  public mutating func move_to<Point :Pointable>(rank: Int) -> Point {
+    .init(file: self.file, rank: self.rank + rank)
+  }
+  
+  public mutating func move_to<Point :Pointable>(point: any Pointable) -> Point {
+    .init(file: self.file + point.file, rank: self.rank + point.rank)
+  }
   
   public func to_bitboard<Bitboard: Bitboardable>() -> Bitboard {
     return { var bitboard = Bitboard.init(rawValue: .zero); bitboard.bitset(forFile: self.file, forRank: self.rank); return bitboard }()
