7ae0cd60806e06470141fc391d95d3212a38eeae
usagi <kou1992@gmail.com>
2023-09-20 13:29:36 +0900

    update

--- Sources/SwiftBitboard/Pointable.swift
+++ Sources/SwiftBitboard/Pointable.swift
@@ -5,6 +5,13 @@ public protocol Pointable: Equatable, Comparable, Hashable {
   var rank: Int { get }
 }
 
+extension Pointable {
+  
+  public func to_bitboard<Bitboard: Bitboardable>() -> Bitboard {
+    return { var bitboard = Bitboard.init(rawValue: .zero); bitboard.bitset(forFile: self.file, forRank: self.rank); return bitboard }()
+  }
+}
+
 // MARK: Comparable
 extension Pointable {
   public static func <(lhs: Self, rhs: Self) -> Bool {
@@ -12,12 +19,5 @@ extension Pointable {
       return lhs.file < rhs.file
     }
       return lhs.rank < rhs.rank
-  }
-}
-
-extension Pointable {
-  
-  func to_bitboard<Bitboard: Bitboardable>() -> Bitboard {
-    return { var bitboard = Bitboard.init(rawValue: .zero); bitboard.bitset(forFile: self.file, forRank: self.rank); return bitboard }()
   }
 }
