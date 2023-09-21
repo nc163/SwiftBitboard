cfe0cb0048e3c98371e225765b5b0bf772a7563b
yoshino <kou1992@gmail.com>
2023-09-22 08:03:49 +0900

    update

--- Sources/SwiftBitboard/FixedSizeable.swift
+++ Sources/SwiftBitboard/FixedSizeable.swift
@@ -29,17 +29,17 @@ public extension FixedSizeable {
   }
   
   // 2点が垂直の直線上にあるか  
-  func arePointsVertical(_ point1: Point, _ point2: Point) -> Bool {
+  func arePointsVertical(_ point1: any CoordinaterePresentable, _ point2: any CoordinaterePresentable) -> Bool {
     return point1.file == point2.file
   }
   
   // 2点が水平の直線上にあるか
-  func arePointsHorizontal(_ point1: Point, _ point2: Point) -> Bool {
+  func arePointsHorizontal(_ point1: any CoordinaterePresentable, _ point2: any CoordinaterePresentable) -> Bool {
     return point1.rank == point2.rank
   }
   
   // 2点が直線上にあるかどうかを判定する
-  func arePointsAligned(_ point1: Point, _ point2: Point) -> Bool {
+  func arePointsAligned(_ point1: any CoordinaterePresentable, _ point2: any CoordinaterePresentable) -> Bool {
     return point1.file == point2.file 
       || point1.rank == point2.rank
       || abs(point1.file - point2.file) == abs(point1.rank - point2.rank)
