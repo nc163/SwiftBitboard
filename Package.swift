306846982c5d2a56846f44aede28d71acc092395
usagi <kou1992@gmail.com>
2023-10-01 00:50:21 +0900

    update

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftBitboard",
-    products: [
-        .library(name: "SwiftBitboard", targets: ["SwiftBitboard"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-    ],
-    targets: [
-        .target(name: "SwiftBitboard"),
-        .testTarget(name: "SwiftBitboardUnitTests", dependencies: ["SwiftBitboard"], path: "Tests"),
-    ]
+  name: "SwiftBitboard",
+  products: [
+    .library(name: "SwiftBitboard", targets: ["SwiftBitboard"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
+  ],
+  targets: [
+    .target(name: "SwiftBitboard"),
+    .testTarget(name: "SwiftBitboardUnitTests", dependencies: ["SwiftBitboard"], path: "Tests"),
+  ]
 )
